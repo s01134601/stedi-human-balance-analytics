6fa9a5e3e58f3eb6d0e93e0aa55294546acc77c6
Biying Ling <111700877+s01134601@users.noreply.github.com>
2023-03-08 16:46:37 -0600

    Create customer_landing.sql

---
+++ customer_landing.sql
@@ -0,0 +1,38 @@
+CREATE EXTERNAL TABLE `customer_landing`(
+  `customername` string COMMENT 'from deserializer', 
+  `email` string COMMENT 'from deserializer', 
+  `phone` string COMMENT 'from deserializer', 
+  `birthday` string COMMENT 'from deserializer', 
+  `serialnumber` string COMMENT 'from deserializer', 
+  `registrationdate` bigint COMMENT 'from deserializer', 
+  `lastupdatedate` bigint COMMENT 'from deserializer', 
+  `sharewithresearchasofdate` bigint COMMENT 'from deserializer', 
+  `sharewithpublicasofdate` bigint COMMENT 'from deserializer', 
+  `sharewithfriendsasofdate` bigint COMMENT 'from deserializer')
+PARTITIONED BY ( 
+  `partition_0` string)
+ROW FORMAT SERDE 
+  'org.openx.data.jsonserde.JsonSerDe' 
+WITH SERDEPROPERTIES ( 
+  'paths'='birthDay,customerName,email,lastUpdateDate,phone,registrationDate,serialNumber,shareWithFriendsAsOfDate,shareWithPublicAsOfDate,shareWithResearchAsOfDate') 
+STORED AS INPUTFORMAT 
+  'org.apache.hadoop.mapred.TextInputFormat' 
+OUTPUTFORMAT 
+  'org.apache.hadoop.hive.ql.io.HiveIgnoreKeyTextOutputFormat'
+LOCATION
+  's3://bling-stedi-human-balance-analytics/customer_landing/'
+TBLPROPERTIES (
+  'CrawlerSchemaDeserializerVersion'='1.0', 
+  'CrawlerSchemaSerializerVersion'='1.0', 
+  'UPDATED_BY_CRAWLER'='bling-stedi-crawler', 
+  'averageRecordSize'='285', 
+  'classification'='json', 
+  'compressionType'='none', 
+  'objectCount'='764', 
+  'recordCount'='759702', 
+  'sizeKey'='218865876', 
+  'typeOfData'='file')
+
+
+Feedback
+Language
